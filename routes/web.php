84af6618cf71772a170e30048250cd22f1732c25
aditya <akstr17coding@gmail.com>
2020-09-09 01:19:57 +0530

    seconds

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/personal', 'PersonalController@index');
+Route::get('/personal/{id}', 'PersonalController@show')->middleware('auth');
+
